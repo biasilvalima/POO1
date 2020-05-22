8399d6c4770507c7ed2bb7e40fa642e8aeaa432b
Beatriz <biasilva.lima118@gmail.com>
2020-05-22 13:42:03 -0300

    Personagem criado

---
+++ Personagem.cs
@@ -0,0 +1,16 @@
+namespace POO1
+{
+    public class Personagem
+    {
+        public string nome = "Tony Stark";
+         
+        public int idade = 35;
+
+        public string armadura;
+
+        public string Atacar(){
+            return "O personagem atacou";
+        }
+
+    }
+}
